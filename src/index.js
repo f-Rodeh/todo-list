3ec38dc8aea0a975de33377446fde0b5844f0d9a
f-Rodeh <rodeh.dev@gmail.com>
2023-08-24 15:32:35 -0600

    Add date manager to tasks

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import { Task } from "./task";
 
-const exampleTask = Task('Example', 'This is an example task')
+const exampleTask = Task('Example', 'This is an example task', '2023-09-09')
 
 console.log( 'example task: ', exampleTask )
-
--- src/task.js
+++ src/task.js
@@ -3,6 +3,10 @@ export { Task }
 const Task = ( title, description = '', dueDate, priority = 0 ) => {
 
   let commentManager = CommentManager()
+
+  const dateManager = DateManager()
+  dateManager.setDueDate( dueDate )
+
   let isDone = false;
 
   return Object.assign({
@@ -11,7 +15,8 @@ const Task = ( title, description = '', dueDate, priority = 0 ) => {
     isDone,
     priority
     },
-    commentManager
+    commentManager,
+    dateManager
   )
 }
 
@@ -29,5 +34,30 @@ function CommentManager() {
   return {
     getComments,
     addComment
+  }
+}
+
+function DateManager(){
+  let dueDate = null;
+
+  function setDueDate( input ){
+    const newDate = new Date( input );
+
+    if( dateIsValid(newDate) ) {
+      dueDate = newDate
+    } else console.error('Date not set: Invalid date');
+  }
+  
+  function getDueDate(){
+    return dueDate
+  }
+
+  function dateIsValid( input ){
+    return !isNaN( input );
+  }
+
+  return {
+    setDueDate,
+    getDueDate
   }
 }
