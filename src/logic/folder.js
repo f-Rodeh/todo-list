93244b6bd0d364ad22fba6e328485068c4d77113
f-Rodeh <rodeh.dev@gmail.com>
2023-08-24 17:12:51 -0600

    Add uid to track tasks

--- src/logic/folder.js
+++ src/logic/folder.js
@@ -5,7 +5,9 @@ const Folder = (title) => {
   let tasks = []
 
   function addTask(task) {
-    tasks.push(task)
+    const uid = generateUid( tasks );
+    task.uid = uid;
+    tasks.push(task);
   }
 
   function getTasks(){
@@ -17,4 +19,32 @@ const Folder = (title) => {
     addTask,
     getTasks
   }
+}
+
+function isIdAvailable( array, id ){
+  return array.every((task) => task.id !== id );
+}
+
+function generateUid( idArray ){
+  let output = 'i'
+  output+= randomString(6);
+
+  if( isIdAvailable(idArray, output) ){
+    return output
+  } else {
+    generateUid( idArray )
+  }
+}
+
+function randomString( length ){
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let output = '';
+
+  for (let i = 0; i < length; i++) {
+    const random = Math.random() * (characters.length-1) ;
+    const index = random.toFixed();
+    output += characters[index]
+  }
+
+  return output
 }
