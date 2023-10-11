7903e6d531727b5a1acea19af60e5fef78f87bb8
f-Rodeh <rodeh.dev@gmail.com>
2023-10-11 16:18:59 -0600

    Fix webpack config for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,27 @@
 const path = require('path');
 
- module.exports = {
+module.exports = {
   entry: './src/index.js',
-   output: {
+  output: {
     filename: 'main.js',
-     path: path.resolve(__dirname, 'dist'),
-   },
-   rules: [
-    {
-      test: /\.css$/i,
-      use: ['style-loader', 'css-loader'],
-    },
-    {
-      test: /\.(jpg|jpeg|png|svg|gif)$/i,
-      type: 'asset/resource'
-    },
-    {
-      test: /\.(woff|woff2)$/i,
-      type: 'asset/resource'
-    }
-   ],
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(jpg|jpeg|png|svg|gif)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(woff|woff2)$/i,
+        type: 'asset/resource'
+      }
+    ],
+  },
   // devtool: 'inline-source-map',
   mode: "production"
- };
+};
